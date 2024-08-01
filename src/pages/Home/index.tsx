e7045fe3c7e83f0db8a9d22124d9831995e81f9b
Leonardo Nunes <Leonardomartinha.dev@gmail.com>
2024-08-01 20:19:24 -0300

    Adicionado a lista de moedas

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,7 +1,49 @@
-import styles from './styles.module.css'
+import { BsSearch } from 'react-icons/bs'
+import styles from './home.module.css'
+import { Link } from 'react-router-dom'
 
 export const Home = () => {
     return (
-        <h1>Home</h1>
+        <main className={styles.container} >
+            <form className={styles.form}>
+                <input type="search" placeholder='Digite o nome da moeda... Ex: Bitcoin' />
+                <button type="submit">
+                    <BsSearch size={30} color="#FFF" />
+                </button>
+            </form>
+            <table>
+                <thead>
+                    <tr>
+                        <th scope='col' >Moeda</th>
+                        <th scope='col' >Valor de mercado</th>
+                        <th scope='col' >Preço</th>
+                        <th scope='col' >Volume</th>
+                        <th scope='col' >Mudança 24</th>
+                    </tr>
+                </thead>
+                <tbody id='tbody' >
+                    <tr className={styles.tr} >
+                        <td className={styles.tdLabel} data-label="Moeda" >
+                            <div className={styles.name} >
+                                <Link to="/details/bitcoin">
+                                    <span>bitcoin</span> | BTC
+                                </Link>
+                            </div>
+                        </td>
+                        <td className={styles.tdLabel} data-label="Valor de mercado" >
+                            1T
+                        </td>
+                        <td className={styles.tdLabel} data-label="Preço" >
+                            66432$
+                        </td> <td className={styles.tdLabel} data-label="Volume" >
+                            2b
+                        </td>
+                        <td className={styles.tdProfit} data-label="Mudança 24" >
+                            <span>1.20</span>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+        </main>
     )
 }
